329c9a5633dfecd9d76d1b77cbb3c7db7c47d104
Pedro Markun <pedro@markun.com.br>
2024-08-30 15:32:52 -0300

    separacao interface e logica

--- app.py
+++ app.py
@@ -1,138 +1,70 @@
-import os
-import json
 import streamlit as st
-import weaviate
-from weaviate.auth import AuthApiKey
-from textblob import TextBlob
-from textblob import download_corpora
-from dotenv import load_dotenv
+from weaviate_handler import (
+    import_txt_file,
+    remove_document_from_weaviate,
+    load_schema,
+    get_vectorized_files,
+    get_chunks_by_file_name,
+    schema_exists
+)
 
 # Configura a interface para ocupar a tela wide
 st.set_page_config(layout="wide")
 
-# Carrega as variáveis de ambiente do arquivo .env
-load_dotenv()
+schema, collection_name = load_schema()
 
-weaviate_host = os.getenv("WEAVIATE_HOST")
-weaviate_api_key = os.getenv("WEAVIATE_API_KEY")
-openai_api_key = os.getenv("OPENAI_API_KEY")
-
-if not weaviate_host or not weaviate_api_key or not openai_api_key:
-    st.error("As variáveis WEAVIATE_HOST, WEAVIATE_API_KEY, e OPENAI_API_KEY devem estar configuradas no arquivo .env.")
-else:
-    # Usando AuthApiKey para autenticação via chave API
-    auth_client = AuthApiKey(api_key=weaviate_api_key)
-
-    client = weaviate.Client(
-        url=weaviate_host,
-        auth_client_secret=auth_client,
-        additional_headers={
-            "X-OpenAI-Api-Key": openai_api_key
-        }
-    )
-
-    def generate_chunked_text(text, chunk_size):
-        download_corpora.download_all()
-        blob = TextBlob(text)
-        chunks = []
-        current_chunk = ""
-        for sentence in blob.sentences:
-            current_chunk += sentence
-            if len(current_chunk) > chunk_size:
-                chunks.append(current_chunk)
-                current_chunk = ""
-
-        return chunks
-
-    def schema_exists(class_name):
-        try:
-            client.schema.get(class_name)
-            return True
-        except weaviate.exceptions.UnexpectedStatusCodeException:
-            return False
-
-    schema_path = 'schemas/default.json'
-    if os.path.exists(schema_path):
-        with open(schema_path, 'r') as schema_file:
-            schema = json.load(schema_file)
-            collection_name = schema.get("class", "PositiveLibraryDocument")
+if schema:
+    schema_already_exists = schema_exists(collection_name)
+    if not schema_already_exists:
+        if st.button("Criar Schema"):
+            try:
+                client.schema.create_class(schema)
+                st.success(f"Coleção '{collection_name}' criada com sucesso!")
+            except weaviate.exceptions.UnexpectedStatusCodeException as e:
+                st.error(f"Erro ao criar a coleção: {e}")
     else:
-        st.error("Arquivo de esquema não encontrado. Verifique o caminho e o nome do arquivo.")
-        schema = None
-        collection_name = "PositiveLibraryDocument"
+        st.info(f"O schema para a coleção '{collection_name}' já existe.")
 
-    if schema:
-        schema_already_exists = schema_exists(collection_name)
-        if not schema_already_exists:
-            if st.button("Criar Schema"):
-                try:
-                    client.schema.create_class(schema)
-                    st.success(f"Coleção '{collection_name}' criada com sucesso!")
-                except weaviate.exceptions.UnexpectedStatusCodeException as e:
-                    st.error(f"Erro ao criar a coleção: {e}")
+st.title("Vetorizador de Arquivos TXT para a Biblioteca Positiva da Lex")
+
+collection_name_input = st.text_input("Nome da Coleção no Weaviate", value=collection_name)
+
+uploaded_file = st.file_uploader("Escolha um arquivo TXT para vetorizar", type=["txt"])
+chunk_size = st.number_input("Tamanho dos chunks (número de caracteres)", min_value=1000, max_value=50000, value=10000)
+doc_name = st.text_input("Nome do Documento")
+doc_type = st.text_input("Tipo do Documento (configurável)")
+
+if uploaded_file and doc_name and doc_type and collection_name_input:
+    if st.button("Vetorizar Arquivo"):
+        message = import_txt_file(uploaded_file, chunk_size, doc_name, doc_type, collection_name_input)
+        st.success(message)
+
+# Layout de duas colunas
+col1, col2 = st.columns([1, 2])
+
+with col1:
+    st.header("Arquivos Vetorizados")
+    selected_file = None
+    if collection_name_input:
+        files = get_vectorized_files(collection_name_input)
+        if files:
+            file_names = {file['properties'].get('doc_name', 'Sem nome'): file['id'] for file in files}
+            selected_file = st.selectbox("Selecione um arquivo", options=list(file_names.keys()))
+
+            if selected_file:
+                if st.button("Remover Documento"):
+                    message = remove_document_from_weaviate(selected_file, collection_name_input)
+                    st.success(message)
         else:
-            st.info(f"O schema para a coleção '{collection_name}' já existe.")
+            st.write("Nenhum arquivo vetorizado encontrado.")
 
-    def import_txt_file(file, chunk_size, doc_name, doc_type, collection_name):
-        try:
-            content = file.read().decode('utf-8')
-            chunks = generate_chunked_text(content, chunk_size)
-            
-            with client.batch as batch:
-                for chunk in chunks:
-                    id = batch.add_data_object(
-                        {
-                            "content": chunk,
-                            "doc_name": doc_name,
-                            "doc_type": doc_type
-                        },
-                        class_name=collection_name
-                    )
-            st.success(f"Arquivo '{doc_name}' vetorizado com sucesso em {len(chunks)} chunks!")
-        except Exception as e:
-            st.error(f"Erro ao vetorizar o arquivo: {e}")
-
-    st.title("Vetorizador de Arquivos TXT para a Biblioteca Positiva da Lex")
-
-    collection_name_input = st.text_input("Nome da Coleção no Weaviate", value=collection_name)
-
-    uploaded_file = st.file_uploader("Escolha um arquivo TXT para vetorizar", type=["txt"])
-    chunk_size = st.number_input("Tamanho dos chunks (número de caracteres)", min_value=100, max_value=5000, value=1000)
-    doc_name = st.text_input("Nome do Documento")
-    doc_type = st.text_input("Tipo do Documento (configurável)")
-
-    if uploaded_file and doc_name and doc_type and collection_name_input:
-        if st.button("Vetorizar Arquivo"):
-            import_txt_file(uploaded_file, chunk_size, doc_name, doc_type, collection_name_input)
-
-    # Layout de duas colunas
-    col1, col2 = st.columns([1, 2])
-
-    with col1:
-        st.header("Arquivos Vetorizados")
-        selected_file = None
-        if collection_name_input:
-            try:
-                objects = client.data_object.get(class_name=collection_name_input)
-                files = objects['objects']
-
-                if files:
-                    file_names = {file['properties'].get('doc_name', 'Sem nome'): file['id'] for file in files}
-                    selected_file = st.selectbox("Selecione um arquivo", options=list(file_names.keys()))
-
-                else:
-                    st.write("Nenhum arquivo vetorizado encontrado.")
-            except Exception as e:
-                st.error(f"Erro ao buscar a lista de arquivos: {e}")
-
-    if selected_file:
-        with col2:
-            st.header(f"Chunks de '{selected_file}'")
-            try:
-                selected_file_id = file_names[selected_file]
-                file_chunks = client.data_object.get_by_id(selected_file_id)['properties']['content']
-                for i, chunk in enumerate(file_chunks.split('\n')):
-                    st.write(f"Chunk {i + 1}:")
-                    st.write(chunk)
-            except Exception as e:
-                st.error(f"Erro ao carregar os chunks do arquivo: {e}")
+if selected_file:
+    with col2:
+        st.header(f"Chunks de '{selected_file}'")
+        chunks = get_chunks_by_file_name(selected_file, collection_name_input)
+        if isinstance(chunks, list):
+            for i, chunk in enumerate(chunks):
+                st.write(f"Chunk {i + 1}:")
+                st.write(chunk[0:511] + "...")
+        else:
+            st.error(chunks)
---
+++ weaviate_handler.py
@@ -0,0 +1,137 @@
+import os
+import json
+import weaviate
+from weaviate.auth import AuthApiKey
+from weaviate.classes.query import Filter
+from dotenv import load_dotenv
+import spacy
+
+# Carrega o modelo spaCy para português
+nlp = spacy.load('pt_core_news_sm')
+
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
+
+weaviate_host = os.getenv("WEAVIATE_HOST")
+weaviate_api_key = os.getenv("WEAVIATE_API_KEY")
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+if not weaviate_host or not weaviate_api_key or not openai_api_key:
+    raise ValueError("As variáveis WEAVIATE_HOST, WEAVIATE_API_KEY, e OPENAI_API_KEY devem estar configuradas no arquivo .env.")
+
+auth_client = AuthApiKey(api_key=weaviate_api_key)
+client = weaviate.Client(
+    url=weaviate_host,
+    auth_client_secret=auth_client,
+    additional_headers={"X-OpenAI-Api-Key": openai_api_key}
+)
+
+def generate_chunked_text(text, chunk_size):
+    # Processa o texto com o spaCy para segmentação de sentenças
+    doc = nlp(text)
+    
+    chunks = []
+    current_chunk = ""
+    
+    # Itera sobre as sentenças detectadas pelo spaCy
+    for sent in doc.sents:
+        current_chunk += sent.text + " "  # Adiciona a sentença ao chunk atual
+        if len(current_chunk) > chunk_size:
+            chunks.append(current_chunk.strip())
+            current_chunk = ""
+    
+    # Adiciona o último chunk se ainda houver texto restante
+    if current_chunk:
+        chunks.append(current_chunk.strip())
+    
+    return chunks
+
+def schema_exists(class_name):
+    try:
+        client.schema.get(class_name)
+        return True
+    except weaviate.exceptions.UnexpectedStatusCodeException:
+        return False
+
+def import_txt_file(file, chunk_size, doc_name, doc_type, collection_name):
+    #try:
+    # Salva uma cópia do arquivo no filesystem
+    os.makedirs('data/upload', exist_ok=True)
+    file_path = os.path.join('data/upload', file.name)
+    with open(file_path, 'wb') as f:
+        f.write(file.getbuffer())
+
+    content = file.read().decode('utf-8')
+    chunks = generate_chunked_text(content, chunk_size)
+    
+    with client.batch as batch:
+        for chunk in chunks:
+            batch.add_data_object(
+                {
+                    "content": chunk,
+                    "doc_name": doc_name,
+                    "doc_type": doc_type
+                },
+                class_name=collection_name
+            )
+    return f"Arquivo '{doc_name}' vetorizado com sucesso em {len(chunks)} chunks!"
+    #except Exception as e:
+    #    return f"Erro ao vetorizar o arquivo: {e}"
+
+
+def remove_document_from_weaviate(doc_name, collection_name):
+    try:
+        # Define o filtro para deletar todos os objetos cujo 'doc_name' corresponde ao documento especificado
+        where_filter = {
+            "path": ["doc_name"],
+            "operator": "Equal",
+            "valueText": doc_name
+        }
+
+        # Executa a exclusão de todos os objetos que correspondem ao filtro
+        client.batch.delete_objects(
+            class_name=collection_name,
+            where=where_filter
+        )
+
+        return f"Documento '{doc_name}' e todos os seus chunks removidos com sucesso da coleção '{collection_name}'."
+    except Exception as e:
+        return f"Erro ao remover o documento e seus chunks: {e}"
+
+
+
+def load_schema(schema_path='schemas/default.json'):
+    if os.path.exists(schema_path):
+        with open(schema_path, 'r') as schema_file:
+            schema = json.load(schema_file)
+            collection_name = schema.get("class", "PositiveLibraryDocument")
+        return schema, collection_name
+    return None, "PositiveLibraryDocument"
+
+def get_vectorized_files(collection_name):
+    try:
+        objects = client.data_object.get(class_name=collection_name)
+        return objects.get('objects', [])
+    except Exception as e:
+        return f"Erro ao buscar a lista de arquivos: {e}"
+
+def get_chunks_by_file_name(doc_name, collection_name):
+    try:
+        # Realiza a consulta para buscar todos os objetos cujo 'doc_name' corresponde ao documento especificado
+        response = (
+            client.query
+            .get(collection_name, ["content"])
+            .with_where({
+                "path": ["doc_name"],
+                "operator": "Equal",
+                "valueText": doc_name
+            }).do()
+        )
+
+        # Extrai o conteúdo de todos os chunks
+        
+        chunks = [obj['content'] for obj in response.get('data', {}).get('Get', {}).get(collection_name, [])]
+
+        return chunks
+    except Exception as e:
+        return f"Erro ao carregar os chunks do arquivo: {e}"
